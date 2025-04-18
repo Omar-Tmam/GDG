0071c25ecf715ccd04cfedc10afdd14135a7d7ef
omar <omartmam9@gmail.com>
2025-04-18 20:04:12 +0200

    change ay 7aga

--- animation/lib/main.dart
+++ animation/lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return GetMaterialApp(
       theme: ThemeData.dark(),
-      debugShowCheckedModeBanner: false,
+      debugShowCheckedModeBanner: true,
       home: SplashView(),
     );
   }
